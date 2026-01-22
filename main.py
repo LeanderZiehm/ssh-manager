9bcd99bc148e5748ba84bcd4d045776735f89ffb
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2026-01-22 13:20:46 +0100

    fix: using managed docker volume

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "sqlite:///ssh_keys.db"
+DATABASE_URL = "sqlite:///data/ssh_keys.db"
 
 # SQLAlchemy setup
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
